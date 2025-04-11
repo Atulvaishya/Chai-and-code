656c263081756e21c4724055734b6435b9af9ef1
Atulvaishya <88301685+Atulvaishya@users.noreply.github.com>
2025-04-11 05:20:01 +0000

    02_dataType

---
+++ 02_dataType.js
@@ -0,0 +1,20 @@
+let name = "Atul"
+let age = 33
+let isLoggedIn = false
+let No = null
+
+// alert(3 + 3) // we are use nodejs , not browser
+
+//number
+//bigint
+//string => ""
+//boolean => true/false
+//null
+//undefined
+//symbol =>
+
+
+//object
+
+console.log(typeof No);
+console.log(typeof "Atul")
