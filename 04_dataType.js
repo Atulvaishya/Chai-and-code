e895cb3a82c3a3aa90f885ed4d9dd9234a15682e
Atulvaishya <88301685+Atulvaishya@users.noreply.github.com>
2025-04-13 11:06:22 +0000

    dataType Summary

--- 04_dataType.js
+++ 04_dataType.js
@@ -2,8 +2,45 @@
 
 // 7 types : string, boolean, number, null, undefined, symbole, bigInt
 
+const score = 100
+const scoreValue = 96.0002
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol ('123')
+// console.log(id == anotherId);
+
+
+
+const bigNumber = 32424424424525n
+
+
 // reference ( Non primitive )
 
 // Array, object, functions
+
+
+const heros = ["shaktiman", "naagraj", "doga"] //array
+
+let myObj = {
+    name : "atul",
+    age : 22,
+
+}      //object
+
+const myFunction = function(){
+    // console.log("Hello World");
+
+    
+}
+
+
+//Check Value Of Any Data
+
+console.log(typeof myFunction);
+console.log(typeof outsideTemp);
+
+
 
 // 
