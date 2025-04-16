32ad54a5c4599e66a8dd7649bd0d5020a2222954
Atulvaishya <88301685+Atulvaishya@users.noreply.github.com>
2025-04-16 12:54:20 +0000

    done

---
+++ 05_string.js
@@ -0,0 +1,33 @@
+const name = "atul"
+const last = "vaishya"
+
+console.log(name + last + " 25"); // old method
+
+console.log(`${name} ${last}`); // New Method
+
+const game = new String('AAAtul')
+console.log(game);
+
+// console.log(game.length);
+
+console.log(game.charAt(3));
+
+console.log(game.indexOf(`A`));
+
+console.log(game.toLowerCase());
+
+console.log(game.toUpperCase());
+
+const newGame = "    Atul_vaishya    "
+
+console.log(newGame);
+
+console.log(newGame.trim());
+
+const url = "http//goog  le.com"
+
+console.log(url);
+
+console.log(url.replace('g', '-'));
+
+
---
+++ 06_Numbers_and_Maths.js
@@ -0,0 +1,49 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString());
+
+//console.log(balance.toFixed(1));    // use for decimal nums
+
+const otherNumber = 123.562652
+//console.log(otherNumber.toPrecision(3));   // use for choose digits
+
+const hundreds = 10000000
+
+// console.log(hundreds.toLocaleString());  // Use commas to define nums in forren type
+
+// console.log(hundreds.toLocaleString(`en-IN`));  //indian nums
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+      
+console.log(Math);
+
+const num = -44.9993883
+// console.log(Math.abs(num)); // Possitive
+
+// console.log(Math.round(num)); //Round Of
+
+// console.log(Math.ceil(num)); // Take round of high
+
+// console.log(Math.floor(num)); // Take round of Low
+
+// console.log(Math.min(3, 8, 1, 1, 4))
+
+// console.log(Math.max(8, 6, 7, 1, 9))
+
+console.log(Math.random()); //Generate random nums (>1)
+
+console.log(Math.floor((Math.random() ) * 10 + 0)); // Generate random nums (<1)
+
+const min = 10
+
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
