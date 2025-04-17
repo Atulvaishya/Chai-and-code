d25b6ee167b47740c0d4be881573afffa008cc6c
Atulvaishya <88301685+Atulvaishya@users.noreply.github.com>
2025-04-17 05:57:48 +0000

    Array

---
+++ 2_array.js
@@ -0,0 +1,36 @@
+//array
+
+const myArr = [1, 2 , 3, 4, 5]
+
+console.log(myArr);
+
+console.log(myArr[1]);
+
+//Array Methods
+
+myArr.push(6) // Add values
+
+console.log(myArr);
+
+(myArr.pop());// Remove last value from Array
+
+console.log(myArr);
+
+myArr.unshift(1, 0);
+
+console.log(myArr);
+
+const newArr = myArr.join()
+
+console.log(newArr);
+
+console.log(typeof newArr);
+
+//Slice and Splice method
+
+console.log("A" , myArr);
+
+const mNA = myArr.slice(1, 3)
+
+console.log(mNA);
+
